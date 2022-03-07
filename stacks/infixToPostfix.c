aea016b08818565316c6e9c2a274baf473b21908
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2022-03-07 12:36:50 +0530

    Added stack to store result
    Now, converting this program to get prefix output is easy (i.e less number of changes to be made)

--- stacks/infixToPostfix.c
+++ stacks/infixToPostfix.c
@@ -4,6 +4,9 @@
 char stack[20];
 int top = -1;
 
+char result[20];
+int topr = -1;
+
 void push(char x){
     stack[++top] = x;
 }
@@ -12,35 +15,40 @@ int pop(){
     return stack[top--];
 }
 
-int priority(char x){
-    if (x == '(') return 0;
-    else if (x=='+'||x=='-') return 1;
-    else if (x=='*'||x=='/') return 2;
+void pushres(char x){
+    result[++topr] = x;
 }
 
-int main()
-{
-   
-    char exp[20];
-    char *e, x;
-    printf("Enter expression>> ");
+int priority(char x){
+    if (x=='(') return 0;
+    else if (x=='+' || x=='-') return 1;
+    else if (x=='/' || x=='*') return 2;
+    else if (x=='^' || x=='$') return 3;
+}
+
+void main(){
+    char exp[20], *e, x;
+    printf("Enter infix expression>> ");
     scanf("%s",exp);
     e = exp;
     while(*e!='\0'){
-        if(isalnum(*e)) printf("%c",*e);
+        if (isalnum(*e)) pushres(*e);
         else if (*e=='(') push(*e);
         else if (*e==')'){
-            while((x = pop())!='(') printf("%c",x);
+            while((x=pop())!='(') pushres(x);
         }
         else{
-            while(priority(stack[top])>=priority(*e)) printf("%c",pop());
+            while (priority(stack[top])>=priority(*e)) pushres(pop());
             push(*e);
-        }
+        }        
         e++;
     }
-    while(top!=-1){
-        printf("%c",pop());
+    while(top>=0){
+        pushres(pop());
     }
-    
-    return 0;
+    printf("Postfix expression>> ");
+    for(int i=0; i<=topr; i++){
+        printf("%c", result[i]);
+    }
+    printf("\n");
 }
